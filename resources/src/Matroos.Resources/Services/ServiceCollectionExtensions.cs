c444ca7943ed7cf840a530368990066e64bcb924
Ángel Gómez <aagomezies@gmail.com>
2022-04-13 17:00:39 +0200

    DEV-30 Add configuration service

---
+++ resources/src/Matroos.Resources/Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Matroos.Resources.Services.Interfaces;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Matroos.Resources.Services
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddResourcesServices(this IServiceCollection services)
+        {
+            return services.AddSingleton<IConfigurationService, ConfigurationService>();
+        }
+    }
+}
