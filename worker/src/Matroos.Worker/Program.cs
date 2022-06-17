e9f113149f06bb38cab503dd0fc6f1844405df7a
Ángel Gómez <aagomezies@gmail.com>
2022-06-17 17:39:28 +0200

    #65 Add worker services

--- worker/src/Matroos.Worker/Program.cs
+++ worker/src/Matroos.Worker/Program.cs
@@ -1,3 +1,6 @@
+using Matroos.Worker.Services;
+using Matroos.Worker.Services.Interfaces;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Services
+builder.Services.AddSingleton<IMainService, MainService>();
 
 WebApplication app = builder.Build();
 
