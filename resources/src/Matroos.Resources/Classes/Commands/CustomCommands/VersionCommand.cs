4b80f1aabe9d7bf59bc2bcf58ba9dc684cf9729f
Ángel Gómez <aagomezies@gmail.com>
2022-06-18 20:48:47 +0200

    DEV-72 Implement the VersionCommand
    Closes #72

---
+++ resources/src/Matroos.Resources/Classes/Commands/CustomCommands/VersionCommand.cs
@@ -0,0 +1,20 @@
+﻿using Discord.WebSocket;
+
+using Matroos.Resources.Classes.Bots;
+using Matroos.Resources.Extensions;
+
+namespace Matroos.Resources.Classes.Commands.CustomCommands;
+
+public class VersionCommand : BaseCommand, IRunnableCommand
+{
+    public VersionCommand() : base(CommandType.STATUS, true)
+    {
+        AllowedModes = new() { CommandMode.SINGLE };
+    }
+
+    public async Task Run(DiscordShardedClient client, SocketMessage message, Bot botSettings, UserCommand command)
+    {
+        AssemblyInfo assemblyInfo = AssemblyExtensions.GetAssemblyInfo();
+        await message.Channel.SendMessageAsync($"{assemblyInfo.Product} - v{assemblyInfo.Version}").ConfigureAwait(false);
+    }
+}
---
+++ resources/src/Matroos.Resources/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+namespace Matroos.Resources.Extensions;
+
+public class AssemblyInfo
+{
+    /// <summary>
+    /// Assembly version.
+    /// </summary>
+    public string Version { get; }
+
+    /// <summary>
+    /// Assembly product.
+    /// </summary>
+    public string Product { get; }
+
+    public AssemblyInfo(string version, string product)
+    {
+        Version = version;
+        Product = product;
+    }
+}
+
+public static class AssemblyExtensions
+{
+    public static AssemblyInfo GetAssemblyInfo()
+    {
+        return new(
+            version: Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "0.0.0",
+            product: Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyProductAttribute>()?.Product ?? "Undefined"
+        );
+    }
+}
