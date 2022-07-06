16befbb01589adaad5bf1e03429888bbd6445464
Ángel Gómez <aagomezies@gmail.com>
2022-07-06 18:49:25 +0200

    DEV-95 Add healthcheck endpoints (#99)

---
+++ worker/src/Matroos.Worker/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Matroos.Resources.Extensions;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Matroos.Worker.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public object Get()
+    {
+        AssemblyInfo assemblyInfo = AssemblyExtensions.GetAssemblyInfo();
+        return Ok(new
+        {
+            Ok = true,
+            assemblyInfo.Version,
+        });
+    }
+}
